edc84e5a6deaf268a97873c11f2404a745b17b65
Diya8181 <92017375+Diya8181@users.noreply.github.com>
2021-10-06 08:58:43 -0700

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a number\n");
+    scanf("%d",&a);
+    printf("enetr b number\n");
+    scanf("%d",&b);
+    printf("sum of 2 number:%d",a+b);
+    return 0;
+
+
+}
