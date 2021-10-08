145d5929bdc4cc33367e638a06b69da4c90af06a
Diya8181 <92017375+Diya8181@users.noreply.github.com>
2021-10-07 20:19:27 -0700

    Add files via upload

---
+++ rel-opt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int a=30,b=40;
+   printf("a==b=%d\n",a==b);
+   printf("a<=b=%d\n",a<=b);
+   printf("a>=b=%d\n",a>=b) ;
+   printf("a<b=%d\n",a<b);
+   printf("a!=b=%d\n",a!=b);
+   printf("a>b=%d\n",a>b);
+   
+   return 0;
+}
