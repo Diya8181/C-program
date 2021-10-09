96678762aa783ad61d00dce38fdc3d3088574572
Diya8181 <92017375+Diya8181@users.noreply.github.com>
2021-10-09 00:38:58 -0700

    Add files via upload

---
+++ nested-if-else.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a=30,b=40,c=50;
+
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("%d",a);
+        }
+        else
+        {
+            printf("%d",c);
+        }
+    }
+    else
+    {
+        if(b>c)
+        {
+            printf("%d",b);
+        }
+        else
+        {
+            printf("%d",c);
+        }
+    }
+    return 0;
+    
+}
