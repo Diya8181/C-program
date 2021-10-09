02472a11fea0113f459163a0e3b63310ea9471c8
Diya8181 <92017375+Diya8181@users.noreply.github.com>
2021-10-08 20:54:58 -0700

    Add files via upload

---
+++ if.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int password;
+    printf("Enter password:");
+    scanf("%d",&password);
+
+    if (password==8127)
+    {
+        printf("welcome diya\n");
+    } 
+        printf("thank you");
+}
